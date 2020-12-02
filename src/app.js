81b4713f250e27d0a0b4569613a02f2ddad184bf
Raphael Sampaio <sampaiodeveloper@gmail.com>
2020-12-02 18:27:24 -0300

    Desafio: Conceitos do Node.js
    - Resolução do desafio finalizada.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index === -1) {
+    return response.status(400).json({ error: 'Repository does not exists.' })
+  }
+
+  const repository = {
+    id,
+    title: title,
+    url: url,
+    techs: techs,
+    likes: repositories[index].likes,
+  };
+
+  repositories[index] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if(index > -1) {
+    repositories.splice(index, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index === -1) {
+    return response.status(400).json({ error: 'Repository does not exists.' })
+  }
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
